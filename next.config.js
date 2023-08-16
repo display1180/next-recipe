c2339da296ae7ad195f57225b308a343dd760dc8
nikji <display1180@gmail.com>
2023-08-16 09:58:07 +0900

    start

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePath: [path.join(__dirname, 'styles')],
+		prependDat: `@import "styles/_variables.scss";`,
+	},
+	images: {
+		domains: ['www.themealdb.com'],
+	},
+	compiler: { styledComponents: true },
+};
+
+module.exports = nextConfig;
